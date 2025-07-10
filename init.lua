c489b79c6062fc8dab82b1646bef3b8c7778c129
Brendon Felix <brendon.felix@hp.com>
2025-07-09 18:07:47 -0600

    update scroll

--- init.lua
+++ init.lua
@@ -853,11 +853,28 @@ require('lazy').setup({
       require('mini.comment').setup()
 
       local animate = require 'mini.animate'
+      local mouse_scrolled = false
+      for _, scroll in ipairs { "Up", "Down" } do
+        local key = '<ScrollWheel' .. scroll .. '>'
+        vim.keymap.set({ "", "i" }, key, function()
+          mouse_scrolled = true
+          return key
+        end, { expr = true })
+      end
       animate.setup {
         cursor = { enable = false },
         scroll = {
           enable = true,
           timing = animate.gen_timing.linear { duration = 100, unit = 'total' },
+          subscroll = animate.gen_subscroll.equal {
+            predicate = function(total_scroll)
+              if mouse_scrolled then
+                mouse_scrolled = false
+                return false
+              end
+              return total_scroll > 1
+            end,
+          },
         },
       }
 
