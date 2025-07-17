8c4085bf5cfc5ab99fb9de993f1a0e08b6041288
Brendon Felix <brendon.felix.c@gmail.com>
2025-07-16 23:08:04 -0600

    quick update

--- init.lua
+++ init.lua
@@ -44,13 +44,18 @@ vim.opt.listchars = {
 -- -------------------------------- keymaps --------------------------------- --
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = '[Q]uickfix list' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, {
+  desc = '[Q]uickfix list',
+})
 
 -- ------------------------------ autocommands ------------------------------ --
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup(
+    'kickstart-highlight-yank',
+    { clear = true }
+  ),
   callback = function()
     vim.hl.on_yank()
   end,
@@ -61,7 +66,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local out = vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--branch=stable',
+    lazyrepo,
+    lazypath,
+  }
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
